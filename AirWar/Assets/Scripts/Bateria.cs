57ec3004c61205801db02cb7db83a77148afa4fc
fabiola-alston <f.alston.1@estudiantec.cr>
2024-11-12 21:20:22 -0600

    added battery

--- AirWar/Assets/Scripts/Bateria.cs
+++ AirWar/Assets/Scripts/Bateria.cs
@@ -6,5 +6,21 @@ public class Bateria : MonoBehaviour
 {
     public int direction;
     public float speed;
+    public Vector3 position;
 
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private Vector3 ReturnPos(Vector3 position)
+    {
+        Vector3 currentPosition = position;
+        return currentPosition;
+    }
 }
