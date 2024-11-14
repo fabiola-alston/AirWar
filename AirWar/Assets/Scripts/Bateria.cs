abe406af406ca82d0b42298a23fa7e50217a29e5
fabiola-alston <f.alston.1@estudiantec.cr>
2024-11-14 17:17:19 -0600

    bullet working

--- AirWar/Assets/Scripts/Bateria.cs
+++ AirWar/Assets/Scripts/Bateria.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class Bateria : MonoBehaviour
 {
-    public int direction;
-    public float speed;
-    public Vector3 position;
-
-    private void Start()
-    {
-
-    }
+    public GameObject bulletPrefab;
+    private GameObject currentBullet;
+    private float speed = 10.0f;
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && currentBullet == null)
+        {
+            Debug.Log("Space bar pressed. Position: " + transform.position);
+            currentBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        }
+
+        if (currentBullet != null)
+        {
+            BulletMove();
+        }
     }
 
-    private Vector3 ReturnPos(Vector3 position)
+    private void BulletMove()
     {
-        Vector3 currentPosition = position;
-        return currentPosition;
+        currentBullet.transform.position += Vector3.up * speed * Time.deltaTime;
+        
+        if (!IsOnScreen(currentBullet))
+        {
+            Destroy(currentBullet);
+            currentBullet = null;   
+        }
     }
+
+    private bool IsOnScreen(GameObject gameObj)
+    {
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(gameObj.transform.position);
+        return screenPoint.y >= 0 && screenPoint.y <= 1 && screenPoint.x >= 0 && screenPoint.x <= 1;
+    }
+
 }
